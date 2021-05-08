b9d3b54e54abb709cd63e4ade8ca534d155805d8
Calvin Hu <31859826+chu017@users.noreply.github.com>
2021-05-08 09:25:51 -0700

    etl in progress

--- helper/writeToDatabase.js
+++ helper/writeToDatabase.js
@@ -2,12 +2,24 @@ const db = require('../database/index.js');
 
 const writeToDatabase = (data) => {
   let query =
-  "INSERT INTO reviews (id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) VALUES ?";
+  "INSERT INTO characteristic_reviews (id,characteristic_id,review_id,value) VALUES ?";
+
 
   db.query(query, [data], (error, response) => {
-    console.log(error);
+    if (error) {
+      console.log(error);
+    }
     // console.log(error || response);
   });
 }
 
 module.exports = writeToDatabase;
+
+/*
+"INSERT INTO reviews (id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) VALUES ?";
+
+"INSERT INTO characteristics (id,product_id,name) VALUES ?";
+
+"INSERT INTO reviews_photos (id,review_id,url) VALUES ?";
+
+*/
